d139894f34f62bcbeeffe753a1cfa9de793b3750
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-23 17:24:10 +0200

    [UPD] show type

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,6 +10,7 @@
 
 <div class="m-3">
     <h2>{{$project['title']}}</h2>
+    <p><strong>Type: </strong>{{$project->type?->title ?: "Undefined"}}</p>
     @if ($project->cover_img)
     <img src="{{asset('storage/' . $project->cover_img)}}" alt="">
     @endif
