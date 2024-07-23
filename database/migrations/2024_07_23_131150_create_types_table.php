a58eb9fb4b7558769cf73a080d7311c6d4c80d67
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-23 15:38:18 +0200

    [UPD] migration types

--- database/migrations/2024_07_23_131150_create_types_table.php
+++ database/migrations/2024_07_23_131150_create_types_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 20)->unique();
+            $table->string('slug', 20);
             $table->timestamps();
         });
     }
